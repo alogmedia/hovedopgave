4863a857b3acbff23b0220c38fd0d6053ae11eeb
allo-mir <allo@mir-robots.com>
2024-10-29 10:53:42 +0100

    chore: code structure

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
 
+  ssr: true,
+
   nitro: {
-    preset: "cloudflare-pages"
+    preset: "cloudflare-pages",
   },
 
-  modules: ["nitro-cloudflare-dev"]
-})
+  modules: ["nitro-cloudflare-dev"],
+
+  vite: {
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@use "~/assets/_colors.scss" as *;',
+        },
+      },
+    },
+  },
+  app: {
+    pageTransition: { name: "page", mode: "out-in" },
+  },
+});
