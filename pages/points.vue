189dd99518a9d7a857afe0573d8197d6ffbe8b93
alogmedia <albertlogn@gmail.com>
2024-11-19 12:32:43 +0100

    fix: nuxt config and points

---
+++ pages/points.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="points">
+    <moleculesTopNav />
+    <organismsPromoBanner />
+    <organismsBottomNav :homeIcon="homeImg"/>
+  </div>
+
+</template>
+  
+<script setup>
+import heroImage from "@/assets/images/hero.png";
+import categoryImage from "@/assets/images/padelbat.png";
+import homeImg from "/icon.png";
+
+</script>
+  
+<style scoped lang="scss">
+@use "@/assets/global_vars.scss" as *;
+
+.points {
+  background-color: $background-color; // Apply specific styles for points page
+}
+
+</style>
+  
