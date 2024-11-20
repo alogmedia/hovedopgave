26b13b8f0d28352eb78d336eba7ea8cd8d5108ed
allo-mir <allo@mir-robots.com>
2024-11-20 17:52:26 +0100

    fix: 3d model product

---
+++ pages/produkt.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="points">
+    <SubHeader />
+    <SmallPromoBanner />
+
+    <!-- Show the 3D model here -->
+    <ThreeProductViewer />
+
+    <BottomNav />
+  </div>
+</template>
+
+<script setup>
+import ThreeProductViewer from "@/components/templates/ThreeProductViewer.vue";
+</script>
+
+<style scoped lang="scss">
+@use "@/assets/global_vars.scss" as *;
+
+.points {
+  background-color: $background-color; // Apply specific styles for points page
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
